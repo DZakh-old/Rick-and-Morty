5a3fca8124e1ea2970a28cbc90be3b2cc53c3eb7
DZakh <dzakh.dev@gmail.com>
2019-11-24 16:20:53 +0300

    Fetching data

--- src/App.js
+++ src/App.js
@@ -6,28 +6,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Shrek'
-        },
-        {
-          name: 'Ice cube'
-        },
-        {
-          name: 'Yellow'
-        }
-      ]
+      monsters: []
     };
   }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+      const monsters = await res.json();
+      this.setState({ monsters });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World!</p>
+          <h1>World Monsters!</h1>
           <ul>
             {this.state.monsters.map(monster => (
-              <li key={monster.name}>{monster.name}</li>
+              <li key={monster.id}>{monster.name}</li>
             ))}
           </ul>
         </header>
