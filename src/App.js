c3c2f3d8d41ae6532cdb5d5cc15bc04e996ac180
DZakh <dzakh.dev@gmail.com>
2019-11-25 13:11:02 +0300

    Very weird search implementation with all available characters

--- src/App.js
+++ src/App.js
@@ -9,38 +9,72 @@ class App extends Component {
     super();
     this.state = {
       characters: [],
-      searchBarValue: ''
+      filteredCharacters: [],
+      searchBarValue: '',
+      nextApiPage: 'https://rickandmortyapi.com/api/character/'
     };
   }
 
-  async componentDidMount() {
+  updateFilteredCharacters = () => {
+    const { characters, searchBarValue } = this.state;
+    this.setState({
+      filteredCharacters: searchBarValue
+        ? [
+            ...characters.filter(character =>
+              character.name.toLowerCase().includes(searchBarValue.toLowerCase())
+            )
+          ]
+        : [...characters]
+    });
+  };
+
+  loadCharacters = async () => {
+    const { nextApiPage, characters } = this.state;
+    if (nextApiPage === ':') return;
     try {
-      const res = await fetch('https://rickandmortyapi.com/api/character');
+      const res = await fetch(nextApiPage);
       if (!res.ok) {
         throw Error(res.statusText);
       }
-      const characters = await res.json();
-      this.setState({ characters: characters.results });
+      const { results, info } = await res.json();
+      await this.setState({
+        characters: [...characters, ...results],
+        nextApiPage: info.next
+      });
+      this.updateFilteredCharacters();
     } catch (err) {
       console.error(err);
     }
+  };
+
+  handleChange = e => {
+    this.setState({ searchBarValue: e.target.value });
+  };
+
+  async componentDidMount() {
+    await this.loadCharacters();
+  }
+
+  async shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.searchBarValue !== nextState.searchBarValue) {
+      this.updateFilteredCharacters();
+      return true;
+    }
+    if (nextState.nextApiPage !== ':' && nextState.filteredCharacters.length < 20) {
+      await this.loadCharacters();
+      return true;
+    }
+    return false;
   }
 
   render() {
-    const { characters, searchBarValue } = this.state;
-    const filteredCharacters = characters.filter(character =>
-      character.name.toLowerCase().includes(searchBarValue.toLowerCase())
-    );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>RICK AND MORTY</h1>
-          <SearchBar
-            placeholder="Search characters"
-            handleChange={e => this.setState({ searchBarValue: e.target.value })}
-          />
-          <CardList characters={filteredCharacters} />
+          <SearchBar placeholder="Search characters" handleChange={this.handleChange} />
+          <CardList characters={this.state.filteredCharacters} />
         </header>
       </div>
     );
